0f5c6319338b3eafa9f34606fcf2b935af659bd7
Deepikappu <151032143+Deepikappu@users.noreply.github.com>
2024-06-12 12:40:01 +0530

    Create lab5a.jl

---
+++ lab5a.jl
@@ -0,0 +1,33 @@
+function lettersOnlyLower(phrase)
+word=""
+for ch in phrase
+if isletter(ch)
+word*lowercase(ch)
+end
+end
+return word
+end
+function isPalindrome(phrase)
+word=lettersOnlyLower(phrase)
+lo=1
+hi=length(word)
+while lo<hi
+if word[lo]!=word[hi] return false end
+lo+=1; hi=1
+end
+return true
+end
+function main()
+print("\nWord or phase?(To stop, press Enter):") 
+phrs=readline()
+while phrs !=""
+if isPalindrome(phrs)
+println("is a palindrome")
+else
+println("is not a palindrome")
+end
+print("\nWordd or phrase?(To stop,press Enter):")
+phrs=readline()
+end
+end
+main()
